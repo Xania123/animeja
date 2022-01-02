fa7eaad6a3bd794dccb96b4a2eac8582cea025de
Xania123 <xania.richards95@gmail.com>
2022-01-01 19:59:23 -0500

    added profile pic
    added profile picture
    updated header footer

--- includes/header.php
+++ includes/header.php
@@ -15,56 +15,34 @@
     <title>Animeja - <?php echo $title ?></title>
   </head>
   <body>
-    <div class= "container">
-     <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-      <a class="navbar-brand" href="index.php">AnimeJA</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button> 
-      
-      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-        <div class="navbar-nav mr-auto">
-          <a class="nav-item nav-link active" href="index.php">Home <span class="sr-only">(current)</span></a>
-          <a class="nav-item nav-link" href="viewrecords.php">View user</a>
-        </div>
-        
-      <div class="navbar-nav ml-auto">
-          <?php 
-              if(!isset($_SESSION['userid'])){
-          ?>
-            <a class="nav-item nav-link" href="login.php">Login <span class="sr-only">(current)</span></a>
-          <?php } else { ?>
-            <a class="nav-item nav-link" href="#"><span>Ohaiyo <?php echo $_SESSION['username'] ?>-san! </span> <span class="sr-only">(current)</span></a>
-            <a class="nav-item nav-link" href="logout.php">Logout <span class="sr-only">(current)</span></a>
-          <?php } ?>
-        </div>
-      </div>
-    </nav>
-    <!--<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-      <a class="navbar-brand" href="index.php"> AnimeJA </a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
 
-      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-        <div class="navbar-nav mr-auto" >
-          <a class="nav-link active" href="index.php">Home <span class="sr-only">(current)</span></a>
-          <a class="nav-link" href="viewrecords.php">View users</a>
-
-        <div class="navbar-nav ml-auto" >
-          //<?php
-              //if(!isset($_SESSION['user_id'])){
-         ?>
-            <a class="nav-link active" href="login.php"> Login <span class="sr-only">(current)</span></a>
-
-          <?php //} else{?>
-            <a class="nav-item nav-link" href="#"><span>Ohaiyo <?php //echo $_SESSION['username'] ?>! </span> <span class="sr-only">(current)</span></a>
-            <a class="nav-link active" href="logout.php"> Logout <span class="sr-only">(current)</span></a>
-
-          <?php //}?>
-        </div>
-      </div>
-    </nav>-->
+      <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+          <a class="navbar-brand" href="index.php">AnimeJA</a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button> 
+          
+          <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav mr-auto">
+              <a class="nav-item nav-link active" href="index.php">Home <span class="sr-only">(current)</span></a>
+              <a class="nav-item nav-link" href="viewrecords.php">View user</a>
+            </div>
+            
+          <div class="navbar-nav ml-auto">
+              <?php 
+                  if(!isset($_SESSION['userid'])){
+              ?>
+                <a class="nav-item nav-link" href="login.php">Login <span class="sr-only">(current)</span></a>
+              <?php } else { ?>
+                <a class="nav-item nav-link" href="#"><span>Ohaiyo <?php echo $_SESSION['username'] ?>-san! </span> <span class="sr-only">(current)</span></a>
+                <a class="nav-item nav-link" href="logout.php">Logout <span class="sr-only">(current)</span></a>
+              <?php } ?>
+            </div>
+          </div>
+        </nav>
+        <div class= "container">
+     
+    
 
     
       
